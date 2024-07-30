03d61df4b18e37f2a894dd81dc6a1f78875fc67d
spiritoftime <chooliangyi6@gmail.com>
2024-07-30 22:59:34 +0800

    added question1.mdx

--- open-math-prep/app/page.tsx
+++ open-math-prep/app/page.tsx
@@ -2,25 +2,25 @@ import { HomeLayout } from "fumadocs-ui/home-layout";
 import Link from "next/link";
 import { baseOptions } from "./layout.config";
 import Test from "@/components/ui/test";
+// import MarblesProblem from "@/components/ui/model";
 
 export default function HomePage() {
-
-
   return (
     <HomeLayout {...baseOptions}>
-      <main className="flex h-screen flex-col justify-center text-center">
+      <main className="flex flex-col justify-center h-screen text-center">
         <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
         <p className="text-muted-foreground">
           You can open{" "}
           <Link
             href="/docs/lectures"
-            className="text-foreground font-semibold underline"
+            className="font-semibold underline text-foreground"
           >
             /docs/lectures
           </Link>{" "}
           and see the documentation.
         </p>
         <Test />
+        {/* <MarblesProblem data={ratioData} /> */}
       </main>
     </HomeLayout>
   );
