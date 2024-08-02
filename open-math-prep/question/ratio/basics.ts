c0649d0cbcbfd08a681e8fd654ac3e0be74d8693
unknown <chooliangyi6@gmail.com>
2024-08-02 14:54:05 +0800

    add ratio basics

---
+++ open-math-prep/question/ratio/basics.ts
@@ -0,0 +1,96 @@
+import { QuestionType } from "@/components/question/multipleChoice";
+
+export const fractionRatioQuestion1Data: QuestionType = {
+  question:
+    "Which of the following represents the same quantity as the ratio 3:4?",
+  options: ["3/4", "4/3", "6/8", "2/3"],
+  answer: "3/4",
+};
+
+export const fractionRatioQuestion2Data: QuestionType = {
+  question:
+    "If the ratio of apples to oranges is 5:3, what fraction represents the proportion of apples to the total number of fruits?",
+  options: ["5/8", "3/5", "3/8", "5/3"],
+  answer: "5/8",
+};
+
+export const fractionRatioQuestion3Data: QuestionType = {
+  question: "What is the ratio equivalent to the fraction 7/10?",
+  options: ["7:10", "10:7", "14:10", "10:14"],
+  answer: "7:10",
+};
+
+export const wordProblemQuestion1Data: QuestionType = {
+  question:
+    "A recipe requires a ratio of 2 cups of sugar to 5 cups of flour. If you want to use 10 cups of flour, how many cups of sugar are needed?",
+  options: ["4 cups", "5 cups", "6 cups", "12 cups"],
+  answer: "12 cups",
+};
+
+export const wordProblemQuestion2Data: QuestionType = {
+  question:
+    "The ratio of cats to dogs in a pet store is 3:2. If there are 30 cats, how many dogs are there?",
+  options: ["20", "15", "25", "18"],
+  answer: "20",
+};
+
+export const wordProblemQuestion3Data: QuestionType = {
+  question:
+    "A group of students is divided into two groups in the ratio 4:5. If there are 36 students in total, how many students are in each group?",
+  options: [
+    "16 in the first group and 20 in the second group",
+    "20 in the first group and 16 in the second group",
+    "18 in each group",
+    "12 in the first group and 24 in the second group",
+  ],
+  answer: "16 in the first group and 20 in the second group",
+};
+
+export const replacementQuestion1Data: QuestionType = {
+  question:
+    "A school bus can either sit 24 adults or 36 children. If it already has 12 adults on the bus, how many more children can it sit?",
+  options: ["12", "18", "24", "30"],
+  answer: "18",
+};
+
+export const replacementQuestion2Data: QuestionType = {
+  question:
+    "A tank can be filled by pipe A in 5 hours and by pipe B in 8 hours. If both pipes are open together, how long will it take to fill the tank?",
+  options: ["3.2 hours", "4 hours", "4.6 hours", "6 hours"],
+  answer: "3.2 hours",
+};
+
+export const replacementQuestion3Data: QuestionType = {
+  question:
+    "A factory produces 60 widgets per hour. If 3 machines are working, how many widgets will be produced in 4 hours?",
+  options: ["180 widgets", "240 widgets", "300 widgets", "720 widgets"],
+  answer: "720 widgets",
+};
+
+export const replacementQuestion4Data: QuestionType = {
+  question:
+    "A boat can travel 120 miles in 4 hours. How far can it travel in 7 hours at the same speed?",
+  options: ["140 miles", "210 miles", "240 miles", "300 miles"],
+  answer: "210 miles",
+};
+
+export const replacementQuestion5Data: QuestionType = {
+  question:
+    "A mixer can blend 40 liters of juice in 15 minutes. How many liters of juice can it blend in 1 hour?",
+  options: ["80 liters", "120 liters", "160 liters", "200 liters"],
+  answer: "160 liters",
+};
+
+export const replacementQuestion6Data: QuestionType = {
+  question:
+    "If a car can travel 150 kilometers on 10 liters of fuel, how many liters of fuel are needed to travel 375 kilometers?",
+  options: ["15 liters", "20 liters", "25 liters", "30 liters"],
+  answer: "25 liters",
+};
+
+export const replacementQuestion7Data: QuestionType = {
+  question:
+    "A box can hold 18 liters of liquid. If a shipment contains 144 liters of liquid, how many boxes are needed to hold all the liquid?",
+  options: ["6 boxes", "7 boxes", "8 boxes", "9 boxes"],
+  answer: "8 boxes",
+};
