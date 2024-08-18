2e4a3a3fef740c356ea07215d261dd3f628a8c95
unknown <chooliangyi6@gmail.com>
2024-08-18 19:47:46 +0800

    add compound percentage

---
+++ open-math-prep/question/percentage/compoundPercentage.ts
@@ -0,0 +1,29 @@
+import { QuestionType } from "@/components/question/multipleChoice";
+
+export const compoundPercentageQuestion1Data: QuestionType = {
+  question:
+    "A bottle contains 500 mL of juice. 80% of the juice is water. During evaporation, 50% of the water content is lost. What is the final volume of juice in the bottle?",
+  options: ["100 mL", "200 mL", "300 mL", "400 mL"],
+  answer: "300 mL",
+};
+
+export const compoundPercentageQuestion2Data: QuestionType = {
+  question:
+    "A fabric weighs 400 grams. 70% of the fabric is cotton. After washing, 30% of the cotton is lost. What is the final weight of the fabric?",
+  options: ["280 grams", "294 grams", "310 grams", "320 grams"],
+  answer: "310 grams",
+};
+
+export const compoundPercentageQuestion3Data: QuestionType = {
+  question:
+    "A box contains 250 grams of mixed nuts. 40% of the mixture is almonds. During storage, 25% of the almonds are consumed by pests. What is the final weight of almonds left in the box?",
+  options: ["30 grams", "40 grams", "60 grams", "75 grams"],
+  answer: "75 grams",
+};
+
+export const compoundPercentageQuestion4Data: QuestionType = {
+  question:
+    "A 1-liter solution contains 90% alcohol. After a chemical reaction, 40% of the alcohol is converted into another substance. What is the final volume of alcohol in the solution?",
+  options: ["0.36 liters", "0.45 liters", "0.54 liters", "0.60 liters"],
+  answer: "0.54 liters",
+};
