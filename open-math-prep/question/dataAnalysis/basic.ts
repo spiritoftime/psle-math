551030c0f14d5b7847e135f51ee55b3b4038c695
unknown <chooliangyi6@gmail.com>
2024-08-28 16:39:37 +0800

    fix question bugs

--- open-math-prep/question/dataAnalysis/basic.ts
+++ open-math-prep/question/dataAnalysis/basic.ts
@@ -5,13 +5,13 @@ export const findingAverageQuestion1Data: QuestionType = {
   question:
     "A set of numbers has a total value of 72 and consists of 8 data points. What is the average of this set?",
   options: ["7", "8", "9", "10"],
-  answer: "C) 9",
+  answer: "9",
 };
 
 export const findingAverageQuestion2Data: QuestionType = {
   question: "If the average of five numbers is 15, what is their total value?",
   options: ["60", "65", "70", "75"],
-  answer: "D) 75",
+  answer: "75",
 };
 
 // 2. Average Relationships
@@ -19,7 +19,7 @@ export const averageRelationshipsQuestion1Data: QuestionType = {
   question:
     "The average score of 4 students is 82. If a new student joins the group and scores 90, what is the new average score of the 5 students?",
   options: ["82", "83", "84", "85"],
-  answer: "B) 83",
+  answer: "83",
 };
 
 export const averageRelationshipsQuestion2Data: QuestionType = {
@@ -31,7 +31,7 @@ export const averageRelationshipsQuestion2Data: QuestionType = {
     "Increases by 5",
     "Increases by 10",
   ],
-  answer: "B) Increases by 3",
+  answer: "Increases by 3",
 };
 
 // 3. Multi-Step Averages
@@ -39,12 +39,12 @@ export const multiStepAveragesQuestion1Data: QuestionType = {
   question:
     "A teacher has two classes. The first class has 12 students with an average score of 75, and the second class has 18 students with an average score of 80. What is the combined average score of the students from both classes?",
   options: ["76", "77", "78", "79"],
-  answer: "C) 78",
+  answer: "78",
 };
 
 export const multiStepAveragesQuestion2Data: QuestionType = {
   question:
     "In a basketball game, a player's average score for the first 4 games is 15 points per game. If the player scores 25 points in the 5th game, what is the new average score?",
   options: ["16", "17", "18", "19"],
-  answer: "B) 17",
+  answer: "17",
 };
