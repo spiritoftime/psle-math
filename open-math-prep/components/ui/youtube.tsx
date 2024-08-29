3f8708ac2d84eb5570bb10fecb7cb12cd7bffee9
unknown <chooliangyi6@gmail.com>
2024-08-29 14:09:12 +0800

    use nocookie for yt embed

--- open-math-prep/components/ui/youtube.tsx
+++ open-math-prep/components/ui/youtube.tsx
@@ -1,12 +1,13 @@
-export default function YouTube ({ id } : { id : string }){
-    return (
-      <div>
-        <iframe
-          className="aspect-video w-full"
-          src={"https://www.youtube.com/embed/" + id}
-          title="YouTube Video Player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        ></iframe>
-      </div>
-    );
-  };
+export default function YouTube({ id }: { id: string }) {
+  return (
+    <div>
+      <iframe
+        className="aspect-video w-full"
+        src={`https://www.youtube-nocookie.com/embed/${id}`}
+        title="YouTube Video Player"
+        loading="lazy"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      ></iframe>
+    </div>
+  );
+}
